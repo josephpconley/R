1cee73ee3ef2e1b92a7f7fe5e9f9d8387bee85a7
Joe Conley <josephpconley@gmail.com>
2014-01-15 21:35:51 -0500

    got a good odds database now from 2008-2013

--- golf/odds.R
+++ golf/odds.R
@@ -1,10 +1,30 @@
-golf <- read.csv("golf/owgr.csv")
-tiger <- subset(golf, name_country=="Tiger Woods United States")
-tiger$yearweek <- with(tiger, year + (week/100))
+#load odds and set field types
+odds <- read.csv("golf/odds.csv")
+odds$name <- as.character(odds$name)
+odds$tournament <- as.character(odds$tournament)
+odds$odds <- as.character(odds$odds)
+odds$won <- as.character(odds$won)
+odds$payoff <- as.numeric(odds$payoff)
 
- Simple Scatterplot
-attach(mtcars)
-plot(tiger$yearweek, tiger$rank, main="Tiger's World Rank 2008-2014", 
- xlab="Year.Week", ylab="Tiger World Rank", pch=19)
+#biggest payoff
+wins <- subset(odds, won=="true")
+max(wins$payoff)
+wins.best <- wins[rev(order(wins$payoff)),]
 
+#field stats
+field <- subset(odds, grepl("Field", name) == 1)
+field$result <- with(field, ifelse(grepl("true", won), 100 * payoff, -100))
 
+field.wins <- subset(field, grepl("true", won) == 1)
+
+#historical winning probability of the field
+nrow(field.wins)/nrow(field)
+
+#find outcome if I bet $100 on field each time
+sum(field$result)
+
+#average odds for the field
+mean(field$payoff)
+
+#best payout
+max(field$result)
