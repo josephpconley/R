5f3b17c1e7679b2187f4ccc3cf58bb18200d982e
Joe Conley <josephpconley@gmail.com>
2014-01-22 11:39:50 -0500

    started hmwk3

--- compdata/week3/hmwk3.R
+++ compdata/week3/hmwk3.R
@@ -5,3 +5,34 @@ outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
 head(outcome)
 ncol(outcome)
 nrow(outcome)
+
+outcome[, 11] <- as.numeric(outcome[, 11])
+outcome[, 17] <- as.numeric(outcome[, 17])
+outcome[, 23] <- as.numeric(outcome[, 23])
+## You may get a warning about NAs being introduced; that is okay
+hist(outcome[, 11], xlab="30-day Death Rate", main="Heart Attack 30-day Death Rate")
+
+
+#obtain 30-day death rate from heart attack, heart failure, pneumonia
+#11
+summary(outcome$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack)
+#17
+summary(outcome$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure)
+#23
+summary(outcome$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia)
+
+
+par(mfrow = c(3, 1))
+hist(outcome[, 11], xlab="30-day Death Rate", xlim=range(10,20), main="Heart Attack")
+hist(outcome[, 17], xlab="30-day Death Rate", xlim=range(10,20), main="Heart Failure")
+hist(outcome[, 23], xlab="30-day Death Rate", xlim=range(10,20), main="Heart Pneumonia")
+  
+par(mfrow = c(1, 3))
+summary(outcome[,11])
+median(outcome[,11], na.rm=TRUE)
+
+abline(v = median(outcome[,11], na.rm=TRUE), col="red")
+abline(v = median(outcome[,17], na.rm=TRUE))
+abline(v = median(outcome[,23], na.rm=TRUE))
+abline(v = c(10,15,20))
+?abline
