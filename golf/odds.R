88f497773f37eddff4b1570b9f8a1ef74691d16d
Joe Conley <josephpconley@gmail.com>
2014-01-30 12:12:02 -0500

    field stuff

--- golf/odds.R
+++ golf/odds.R
@@ -5,6 +5,7 @@ odds$tournament <- as.character(odds$tournament)
 odds$odds <- as.character(odds$odds)
 odds$won <- as.character(odds$won)
 odds$payoff <- as.numeric(odds$payoff)
+odds$id <- with(odds, paste(year, "-", tournament))
 
 #biggest payoff
 wins <- subset(odds, won=="true")
@@ -14,19 +15,22 @@ head(wins.best)
 
 #field stats
 field <- subset(odds, grepl("Field", name) == 1)
-field$result <- with(field, ifelse(grepl("true", won), 100 * payoff, -100))
 field.wins <- subset(field, grepl("true", won) == 1)
 field.wins.best <- field.wins[rev(order(field.wins$payoff)),]
 head(field.wins.best)
 
+#number of events offering a field bet
+nrow(field)
+
+#number of times the field hit
+nrow(field.wins)
+
 #historical winning probability of the field
 nrow(field.wins)/nrow(field)
 
-#find outcome if I bet $100 on field each time
-sum(field$result)
+#find outcome if I bet $25 on field each time
+field$bet <- with(field, ifelse(grepl("true", won), 25 * payoff, -25))
+sum(field$bet)
 
 #average odds for the field
 mean(field$payoff)
-
-#best payout
-max(field$result)
